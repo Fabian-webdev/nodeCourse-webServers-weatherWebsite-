09a12d28c3776a1ed5b7607e0098c4930d0bca63
Fabian-webdev <lobnig.fabian@gmail.com>
2020-04-27 20:44:59 +0200

    Added additional forecast information (humidity and windspeed)

--- src/utils/getWeather.js
+++ src/utils/getWeather.js
@@ -6,12 +6,12 @@ const getWeather = (latLon, callback)=>{
     const url = `http://api.weatherstack.com/current?access_key=16eaaa66782d1d1332266bdd15b0776f&query=${latLon}${unit}`;
     request({url, json:true}, (e, {body}={})=>{
         try{
-            const {weather_descriptions, temperature, precip} = body.current;
+            const {weather_descriptions, temperature, precip, humidity, wind_speed} = body.current;
             const {country, name:locationName, localtime} = body.location;
             const error = "Unable to find location, please try again";
             if(!weather_descriptions[0]&& !country) callback({error}, undefined)
             else {
-                const response = `It´s ${weather_descriptions[0]} in ${locationName} ${country} at around ${temperature} and the chance of rain is ${precip}. This was measured at ${localtime}`
+            const response = `It´s ${weather_descriptions[0]} in ${locationName} ${country} at around ${temperature} and the chance of rain is ${precip}. This was measured at ${localtime} at a humidity of ${humidity}% and wind speeds up to ${wind_speed} km/h`
                 callback(undefined, {response});
             }
         } 
